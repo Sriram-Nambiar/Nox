13ba8069283a5809bbc6bad9593543f1b72f1352
Sriram-Nambiar <sriramnambiar21@gmail.com>
2026-09-18 18:17:48 +0530

    Refactor Home component layout for improved styling and structure

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,33 +3,39 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [username, setUsername] = useState("");
-  const [data, setData] = useState<any>(null);
+    const [username, setUsername] = useState("");
+    const [data, setData] = useState<any>(null);
 
-  const search = async () => {
-    const res = await fetch(`/api/github?username=${username}`);
-    const result = await res.json();
-    setData(result);
-  };
+    const search = async () => {
+        const res = await fetch(`/api/github?username=${username}`);
+        const result = await res.json();
+        setData(result);
+    };
 
-  return (
-    <div>
-      <input className="m-3"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="GitHub username"
-      />
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="m-5">
+                <input className="bg-amber-50 text-black p-2 rounded-2xl"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="GitHub username"
+                />
+                <button className=" bg-amber-50 text-black p-2 rounded-2xl" onClick={search}>Search</button>
+            </div>
 
-      <button className="m-3" onClick={search}>Search</button>
 
-        {data.name}
-        <img className="rounded-full size-30" src={data.avatar_url}/>
-        <p>Followers</p>
-        {data.followers}
-        <p>Following</p>
-        {data.following}
-       
-      
-    </div>
-  );
+
+            <div className="border-2 p-3">
+                <img className="rounded-full size-30" src={data.avatar_url} />
+                {data.name}
+                <p>Followers</p>
+                {data.followers}
+                <p>Following</p>
+                {data.following}
+            </div>
+
+
+
+        </div>
+    );
 }
