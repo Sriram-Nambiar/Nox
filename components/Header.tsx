5c30fccf80c6010375017069f23d6da2e2e1fd9d
Sriram-Nambiar <sriramnambiar21@gmail.com>
2026-09-20 16:39:40 +0530

    feat: update Header component to include navigation items

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,11 @@
 export default function Header(){
     return(
         <div className="w-100% bg-[#30AFFF] h-10 p-2 ">
-           new
+          <h2>Home</h2>
+        <h2>Levels</h2>
+        <h2>Characters</h2>
+        <h2>Missions</h2>
+        
         </div>
     )
 }
